a4f393cf71f5bbf6b6aa7f636d743f50481f9083
Raphaël Voyazopoulos <raphael.voyazopoulos@bandsquare.com>
2017-07-25 18:58:28 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 var request = require('axios');
+var _ = require('underscore');
+
+var requestConfig = { headers: {} };
+var CONSUMER_KEY = null;
+var CONSUMER_SECRET = null;
+var API_URL = 'https://api.discogs.com';
+
+var setCredentials = function(key, secret) {
+
+  if(!_.isString(key) || !_.isString(secret)) throw new Error('Key and secret must be strings.');
+
+  CONSUMER_KEY = key;
+  CONSUMER_SECRET = secret;
+
+  requestConfig.headers = {
+    'Authorization': 'Discogs key='+CONSUMER_KEY+', secret='+CONSUMER_SECRET
+  };
+};
 
 var fromList = function(listId) {
+
+  if(CONSUMER_KEY === null || CONSUMER_SECRET === null) throw new Error('Discogs API credentials are not set.');
   if(!listId) throw new Error('A list ID must be specified.');
-  request.get('http://google.fr').then(function(res) {
-    console.log('••• res', res);
+
+  return request.get(API_URL+'/lists/'+listId, requestConfig).then(function(res) {
+    return res.data;
+  }).catch(function(err) {
+    throw new Error(err.status+' '+err.statusText);
   });
 };
 
 module.exports = {
-  fromList: fromList
+  fromList: fromList,
+  setCredentials: setCredentials
 };
---
+++ src/test.js
@@ -0,0 +1,5 @@
+var sleeve = require('./index.js');
+sleeve.setCredentials('CXDjOnwBOBrUIPVjpbMh', 'NyFFxQoZSxbmiqWQYwZTKDGQQMJwuUtX');
+sleeve.fromList(352246).then(function(res) {
+  console.log('••• res', res);
+});
