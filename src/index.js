e1bf45f3719d38e01f477866c638299e8729b372
Raphaël Voyazopoulos <raphael.voyazopoulos@bandsquare.com>
2017-08-11 15:42:42 +0200

    First MVP.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,62 @@
-var request = require('axios');
+var axios = require('axios');
 var _ = require('underscore');
 
-var requestConfig = { headers: {} };
-var CONSUMER_KEY = null;
-var CONSUMER_SECRET = null;
-var API_URL = 'https://api.discogs.com';
+var instanceConfig = {
+  baseURL: 'https://api.discogs.com'
+};
 
+var api = axios.create(instanceConfig);
+
+/**
+ * Sets the Discogs API credentials.
+ *
+ * @param      {string}  key     The key
+ * @param      {string}  secret  The secret
+ */
 var setCredentials = function(key, secret) {
 
   if(!_.isString(key) || !_.isString(secret)) throw new Error('Key and secret must be strings.');
 
-  CONSUMER_KEY = key;
-  CONSUMER_SECRET = secret;
-
-  requestConfig.headers = {
-    'Authorization': 'Discogs key='+CONSUMER_KEY+', secret='+CONSUMER_SECRET
-  };
+  api = axios.create(_.extend({}, instanceConfig, { headers: {
+    'Authorization': 'Discogs key='+key+', secret='+secret
+  }}));
 };
 
-var fromList = function(listId) {
+/**
+ * Gets a random sleeve.
+ *
+ * @param      {int}     listId  The list identifier (optional)
+ * @return     {string}  The sleeve URL.
+ */
+var getSleeve = function(listId) {
 
-  if(CONSUMER_KEY === null || CONSUMER_SECRET === null) throw new Error('Discogs API credentials are not set.');
-  if(!listId) throw new Error('A list ID must be specified.');
+  if(!listId) listId = 2056;
 
-  return request.get(API_URL+'/lists/'+listId, requestConfig).then(function(res) {
-    return res.data;
-  }).catch(function(err) {
-    throw new Error(err.status+' '+err.statusText);
+  function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function getList(listId) {
+    return api.get('/lists/' + listId);
+  }
+
+  function getItem(itemId) {
+    return api.get('/masters/' + itemId);
+  }
+
+  return getList(listId).then(function(res) {
+    var rand = random(0, res.data.items.length);
+    return getItem(res.data.items[rand].id).then(function(res) {
+      return res.data.images[0].uri;
+    });
+  }).catch(function(res) {
+    return null;
   });
 };
 
 module.exports = {
-  fromList: fromList,
+  getSleeve: getSleeve,
   setCredentials: setCredentials
 };
