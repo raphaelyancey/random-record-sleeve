ce5b3df417728f48eeb587c4eee1e9fcd981e5b4
Raphaël Voyazopoulos <raphael.voyazopoulos@bandsquare.com>
2017-08-11 16:08:38 +0200

    Deleted test and updated .gitignore.

--- src/test.js
+++
@@ -1,5 +0,0 @@
-var sleeve = require('./index.js');
-sleeve.setCredentials('CXDjOnwBOBrUIPVjpbMh', 'NyFFxQoZSxbmiqWQYwZTKDGQQMJwuUtX');
-sleeve.fromList(352246).then(function(res) {
-  console.log('••• res', res);
-});
